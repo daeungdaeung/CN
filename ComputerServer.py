3639ae0b7363e5e6589dfc5dbafd9e47dce0e3bf
daeungdaeung <41292577+daeungdaeung@users.noreply.github.com>
2018-12-13 16:34:33 +0900

    Add files via upload

--- ComputerServer.py
+++ ComputerServer.py
@@ -1,27 +1,16 @@
 from socket import *
+# For execute python files in python file.
+import subprocess
 
 serverPort = 12006
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(5)
 print('The server is ready to receive')
+
 while True:
     connectionSocket, addr = serverSocket.accept()
+    subprocess.call(['python', 'ComputerClient.py'])
     sentence = connectionSocket.recv(1024).decode()
     print(sentence)
     connectionSocket.close()
-
-    import subprocess
-
-    # For execute python files in python file.
-
-    while True:
-        Input = input('Put: ')
-        if 'quit' == Input:
-            break
-        else:
-            subprocess.call(['python', 'ComputerClient.py'])
-
-
-
-
